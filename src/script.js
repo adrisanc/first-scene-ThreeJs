89f86387e20cc8f77b67b703d773bb7d85b14602
YASMIN ADRIANA ORJUELA SANCHEZ <yasminadrianasanchez@gmail.com>
2023-03-29 21:31:27 -0500

    finsh material testing

--- src/script.js
+++ src/script.js
@@ -6,18 +6,33 @@ import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import * as dat from "dat.gui";
 import { log } from "three";
 
+//Debug ui
+const gui = new dat.GUI();
+
+
 
 //Textures
 const textureLoader = new THREE.TextureLoader();
+const cubeTextureLoader = new THREE.CubeTextureLoader();
 
 const doorColorTexture = textureLoader.load('/textures/door/color.jpg');
 const doorAlphaTexture = textureLoader.load('/textures/door/alpha.jpg');
+const doorHeightTexture = textureLoader.load('/textures/door/height.jpg');
 const doorNormalTexture = textureLoader.load('/textures/door/normal.jpg');
 const doorMetalnessTexture = textureLoader.load('/textures/door/metalness.jpg');
 const doorAmbientOcclusionTexture = textureLoader.load('/textures/door/ambientOcclusion.jpg');
 const doorRoughnessTexture = textureLoader.load('/textures/door/roughness.jpg');
 const matCapTexture = textureLoader.load('/textures/matcaps/7.png');
 const gradientTexture = textureLoader.load('/textures/gradients/3.jpg')
+
+const enviromentMapTexture = cubeTextureLoader.load([
+  '/textures/environmentMaps/4/px.png',
+  '/textures/environmentMaps/4/nx.png',
+  '/textures/environmentMaps/4/py.png',
+  '/textures/environmentMaps/4/ny.png',
+  '/textures/environmentMaps/4/pz.png',
+  '/textures/environmentMaps/4/nz.png',
+])
 
 
 //scene
@@ -67,30 +82,66 @@ material.map = doorColorTexture;
 
   //?CartonMaterial
   const cartoonMaterial = new THREE.MeshToonMaterial();
-  
-  
+  //to sharpen color on mesh
+  gradientTexture.minFilter = THREE.NearestFilter;
+  gradientTexture.magFilter = THREE.NearestFilter;
+  gradientTexture.generateMipmaps = true;
+  cartoonMaterial.gradientMap = gradientTexture;
+
+  //?MeshStandarMaterial
+  const standardMaterial = new THREE.MeshStandardMaterial()
+  // standardMaterial.roughness = 0.90;
+  // standardMaterial.metalness = 0.15;
+  // standardMaterial.metalnessMap = doorMetalnessTexture;
+  // standardMaterial.roughnessMap = doorRoughnessTexture
+  // standardMaterial.map = doorColorTexture;
+  // standardMaterial.aoMap = doorAmbientOcclusionTexture;
+  // standardMaterial.aoMapIntensity = 0.5;
+  // standardMaterial.normalMap = doorNormalTexture;
+  // standardMaterial.normalScale.set(0.5,0.5);
+  // standardMaterial.displacementMap = doorHeightTexture;
+  // standardMaterial.displacementScale = 0.1;
+  // standardMaterial.transparent= true;
+  // standardMaterial.alphaMap = doorAlphaTexture;
+  standardMaterial.envMap = enviromentMapTexture;
+
+  //debug ui parameter for mateial
+  gui.add(standardMaterial, 'metalness').min(0).max(1).step(0.001);
+  gui.add(standardMaterial, 'roughness').min(0).max(1).step(0.001);
+  gui.add(standardMaterial, 'aoMapIntensity').min(0).max(1).step(0.001);
+  gui.add(standardMaterial, 'displacementScale').min(0).max(1).step(0.001);
   
 
 //Sphere
  const sphere = new THREE.Mesh(
-  new THREE.SphereBufferGeometry(0.5,16,16),
-  matCapMaterial
+  new THREE.SphereBufferGeometry(0.5,64,64),
+  standardMaterial
  );
+
+ sphere.geometry.setAttribute('uv2', new THREE.BufferAttribute(sphere.geometry.attributes.uv.array,2))
+
 
  //plane
  const plane = new THREE.Mesh(
-  new THREE.PlaneBufferGeometry(1,1),
-  material
+  new THREE.PlaneBufferGeometry(1,1,100,100),
+  standardMaterial
 
  );
+
+ plane.geometry.setAttribute('uv2', new THREE.BufferAttribute(plane.geometry.attributes.uv.array,2))
+
+  console.log(plane.geometry.attributes.uv);
 
  plane.position.x = 1.3;
 
  //Torus
  const torus = new THREE.Mesh(
-  new  THREE.TorusBufferGeometry(0.3,0.2,16,12),
-    cartoonMaterial
+  new  THREE.TorusBufferGeometry(0.3,0.2,64,128),
+    standardMaterial
  );
+
+ torus.geometry.setAttribute('uv2', new THREE.BufferAttribute(torus.geometry.attributes.uv.array,2))
+
 
  torus.position.x = -1.3;
 
@@ -153,13 +204,13 @@ function animate() {
   const elapsedTime = clock.getElapsedTime();
 
   //animate meshes
-  plane.rotation.y = 0.1 * elapsedTime;
-  torus.rotation.y = 0.1 * elapsedTime;
-  sphere.rotation.y = 0.1 * elapsedTime;
+  // plane.rotation.y = 0.1 * elapsedTime;
+  // torus.rotation.y = 0.1 * elapsedTime;
+  // sphere.rotation.y = 0.1 * elapsedTime;
 
-  plane.rotation.x = 0.3 * elapsedTime;
-  torus.rotation.x = 0.3 * elapsedTime;
-  sphere.rotation.x = 0.3 * elapsedTime;
+  // plane.rotation.x = 0.3 * elapsedTime;
+  // torus.rotation.x = 0.3 * elapsedTime;
+  // sphere.rotation.x = 0.3 * elapsedTime;
   
 
 	renderer.render( scene, camera );
